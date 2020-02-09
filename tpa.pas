a30c02bfc9fc468ddb612048360aae74d74a6d04
asima2002 <45330721+asima2002@users.noreply.github.com>
2020-02-09 17:46:36 +0200

    Add files via upload

---
+++ tpa.pas
@@ -0,0 +1,10 @@
+﻿Program P24;
+var a,b,x,y,D:real;
+function distanta(a1,b1,x1,y1:real):real;
+begin
+distanta:=sqrt(sqr(a1-x1)+sqr(b1-y1));
+end;
+readln(a,b,x,y);
+D:=distanta (a,b,x,y);
+writeln('distanta dintre coordonate='),D);
+end.
